763e9ac27afab59fd6cd22be946c071c1d73683f
dnzhelloo <dnzhelloo@icloud.com>
2023-01-15 23:38:57 +0300

    working with While loops

---
+++ Control Flow.playground/Pages/While loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//While loops
+var number = 1
+
+while number<=20{
+  print(number)
+    number += 1
+}
+print("Ready or not, here I come!")
+
+
+/*
+                                                When should you use a while loop?
+ 
+ This means we can repeat the same code until…
+
+ …the user asks us to stop
+ …a server tell us to stop
+ …we’ve found the answer we’re looking for
+ …we’ve generated enough data
+ 
+ */
